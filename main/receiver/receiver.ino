2397511df35bda60ae181eb36bbeeadfc716c308
m4nzm333 <irman.mashuri@gmail.com>
2022-09-18 16:02:06 +0800

    Menambahkan komentar pada Program Receiver

--- main/receiver/receiver.ino
+++ main/receiver/receiver.ino
@@ -1,18 +1,18 @@
-/*********
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/ttgo-lora32-sx1276-arduino-ide/
-*********/
+/*****************
+ * Program untuk Perangkat Penerima
+ * Perangkat hanya menerima data dari Sender yang dikirim melalui protokol LORA kemudian dikirim di Serial ke Raspberry
+ ****************/
 
-//Libraries for LoRa
+// Library yang digunakan oleh LORA
 #include <SPI.h>
 #include <LoRa.h>
 
-//Libraries for OLED Display
+// Library untuk OLED Display
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-//define the pins used by the LoRa transceiver module
+// PIN GPIO yang digunakan oleh LORA
 #define SCK 5
 #define MISO 19
 #define MOSI 27
@@ -20,18 +20,19 @@
 #define RST 14
 #define DIO0 26
 
-//433E6 for Asia
-//866E6 for Europe
-//915E6 for North America
+// 433E6 for Asia
+// 866E6 for Europe
+// 915E6 for North America
+// Beberapa Paramter yang digunakan oleh LORA seperti Frekuensi, TX Power Antena, Bandwitdth dan lain-lain
 #define BAND 915E6
 #define TXPOWER 20
 #define SIGNAL_BANDWIDTH 7.8E3
 #define SPREADING_FACTOR 12
 #define SIGNAL_GAIN 6
 
-//OLED pins
+// PIN GPIO yang digunakan oleh OLED
 #define OLED_SDA 4
-#define OLED_SCL 15 
+#define OLED_SCL 15
 #define OLED_RST 16
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -40,84 +41,92 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
 
 String LoRaData;
 
-void setup() { 
-  //initialize Serial Monitor
+void setup()
+{
+  // Init Serial Monitor dan Serial Pengiriman
   Serial.begin(9600);
   Serial2.begin(9600, SERIAL_8N1, 16, 17);
-  
-  //reset OLED display via software
+
+  // Reset Tampilan OLED
   pinMode(OLED_RST, OUTPUT);
   digitalWrite(OLED_RST, LOW);
   delay(20);
   digitalWrite(OLED_RST, HIGH);
-  
-  //initialize OLED
+
+  // Init OLED
   Wire.begin(OLED_SDA, OLED_SCL);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) { // Address 0x3C for 128x32
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false))
+  { // Address 0x3C for 128x32
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for (;;)
+      ; // Don't proceed, loop forever
   }
 
+  // Tampilkan nama perangkat OLED
   display.clearDisplay();
   display.setTextColor(WHITE);
   display.setTextSize(1);
-  display.setCursor(0,0);
+  display.setCursor(0, 0);
   display.print("LORA RECEIVER ");
   display.display();
 
   Serial.println("LoRa Receiver Test");
-  
-  //SPI LoRa pins
+
+  // SETUP Semua Perangkat Lora ke Mikrokontroller
   SPI.begin(SCK, MISO, MOSI, SS);
-  //setup LoRa transceiver module
   LoRa.setPins(SS, RST, DIO0);
   LoRa.setTxPower(TXPOWER);
   LoRa.setGain(SIGNAL_GAIN);
   LoRa.setSignalBandwidth(SIGNAL_BANDWIDTH);
   LoRa.setSpreadingFactor(SPREADING_FACTOR);
 
-  if (!LoRa.begin(BAND)) {
+  if (!LoRa.begin(BAND))
+  {
     Serial.println("Starting LoRa failed!");
-    while (1);
+    while (1)
+      ;
   }
   Serial.println("LoRa Initializing OK!");
-  display.setCursor(0,10);
+  display.setCursor(0, 10);
   display.println("LoRa Initializing OK!");
-  display.display();  
+  display.display();
 }
 
-void loop() {
+void loop()
+{
 
-  //try to parse packet
+  // Mencoba untuk konversi paket signal pengirim ke bentuk yang bisa dibaca
   int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    //received a packet
+  if (packetSize)
+  {
+    // Menerima paket data
     Serial.print("Received packet ");
 
-    //read packet
-    while (LoRa.available()) {
+    // Membaca Data
+    while (LoRa.available())
+    {
       LoRaData = LoRa.readString();
       Serial.print(LoRaData);
     }
 
-    //print RSSI of packet
+    // Tampilkan RSSI (Kekuatan Signal Pengirim)
     int rssi = LoRa.packetRssi();
-    Serial.print(" with RSSI ");    
+    Serial.print(" with RSSI ");
     Serial.println(rssi);
 
-   // Dsiplay information
-   display.clearDisplay();
-   display.setCursor(0,0);
-   display.print("LORA RECEIVER");
-   display.setCursor(0,20);
-   display.print("Received packet:");
-   display.setCursor(0,30);
-   display.print(LoRaData);
-   display.setCursor(0,40);
-   display.print("RSSI:");
-   display.setCursor(30,40);
-   display.print(rssi);
-   display.display();
-   Serial2.print(LoRaData);   
+    // Memunculkan isi data ke OLED Penerima
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.print("LORA RECEIVER");
+    display.setCursor(0, 20);
+    display.print("Received packet:");
+    display.setCursor(0, 30);
+    display.print(LoRaData);
+    display.setCursor(0, 40);
+    display.print("RSSI:");
+    display.setCursor(30, 40);
+    display.print(rssi);
+    display.display();
+    Serial2.print(LoRaData);
   }
 }
